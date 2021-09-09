15ca91737d7c1769f9d65a9646058a64d5360194
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-09-09 17:35:45 +0530

    0

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -6,6 +6,8 @@ import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
+logger = logging.getLogger('__name__')
+
 reddit = asyncpraw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, user_agent = USER_AGENT)
 
 loop = asyncio.get_event_loop()
